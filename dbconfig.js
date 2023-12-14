29024785a10607757c708d7517848d7c95020a27
Maan Patel <maanpatel1811@gmail.com>
2023-12-13 19:35:42 -0600

    fixed the bugs

--- dbconfig.js
+++ dbconfig.js
@@ -2,7 +2,7 @@
 dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  user: process.env.DB_USERNAME || 'COMP214_f23_ers_31',
+  user: process.env.DB_USERNAME || 'COMP214_f23_ers_28',
   password: 'password',
   connectString: '199.212.26.208:1521/SQLD',
 };
